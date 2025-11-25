f77478ea8e1c77945d0653a89f0a5db19bd20c17
Okwizi <okwizifr@gmail.com>
2025-11-25 15:13:00 +0300

    feat: add formatting, linting, and type checking

--- huldra/providers/base.py
+++ huldra/providers/base.py
@@ -1,6 +1,5 @@
 """Base Abstract for all Providers."""
 
-
 from abc import ABC, abstractmethod
 from typing import List
 
@@ -9,16 +8,13 @@ from huldra.core.types import Vulnerability
 
 class Provider(ABC):
     """Base Provider class."""
+
     @abstractmethod
     def audit(self) -> List[Vulnerability]:
-        """
-        Run the audit and return a list of vulnerabilities.
-        """
+        """Run the audit and return a list of vulnerabilities."""
         pass
 
     @abstractmethod
     def apply_fix(self, command: str) -> bool:
-        """
-        Apply a fix command.
-        """
+        """Apply a fix command."""
         pass
